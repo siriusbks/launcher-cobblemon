ae63777d1b65b0b87f2e428a6090878c30642bec
Nathan <nath.bordat@hotmail.fr>
2025-04-24 12:52:12 +0200

    minor changes

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -84,7 +84,7 @@ async function setStatus(opt) {
 
     if (!opt) {
         statusServerElement.classList.add('red')
-        statusServerElement.innerHTML = `Offline`
+        statusServerElement.innerHTML = `Hors ligne`
         document.querySelector('.status-player-count').classList.add('red')
         playersOnline.innerHTML = '0'
         return
@@ -102,7 +102,7 @@ async function setStatus(opt) {
         playersOnline.innerHTML = statusServer.playersConnect
     } else {
         statusServerElement.classList.add('red')
-        statusServerElement.innerHTML = `Offline`
+        statusServerElement.innerHTML = `Hors ligne`
         /* document.querySelector('.status-player-count').classList.add('red') */
         playersOnline.innerHTML = '0'
     }
